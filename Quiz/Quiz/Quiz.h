98ebc79e0829aba54908eb3ab83fafbf8fa626c4
Austinlsink <austinsink1997@gmail.com>
2020-04-25 09:25:41 -0400

    Quiz app

---
+++ Quiz/Quiz/Quiz.h
@@ -0,0 +1,19 @@
+//
+//  Quiz.h
+//  Quiz
+//
+//  Created by Austin Sink on 4/24/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Quiz.
+FOUNDATION_EXPORT double QuizVersionNumber;
+
+//! Project version string for Quiz.
+FOUNDATION_EXPORT const unsigned char QuizVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Quiz/PublicHeader.h>
+
+
