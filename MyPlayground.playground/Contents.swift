2385e3d2e4cacdf17e5e5d7895d6384dbc14d3b7
Austinlsink <austinsink1997@gmail.com>
2020-04-26 19:17:23 -0400

    Update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
 var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+
+var responseMessages = [200: "OK",
+403: "Access forbidden",
+404: "File not found",
+500: "Internal server error"]
+
+responseMessages[500]
