66332b7768c28560a359603cd34726d36a65321c
Austin Sink <austinsink1997@gmail.com>
2020-06-07 09:33:32 -0400

    WorldTrotter update

---
+++ Quiz 2/Quiz 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz 2
+//
+//  Created by Austin Sink on 5/17/20.
+//  Copyright © 2020 Tech in Sync. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,6 +11,78 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    //
+    // Everytime fahrenheit value changes it will call
+    // updateCelsiusLabel()
+    //
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    //
+    // Update celsius label on app load
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+    
+    //
+    // Format calculated calsius value
+    //
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    } ()
+    
+    //
+    // Convert fahrenheit to celsius
+    //
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        else {
+            return nil
+        }
+    }
 
-    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) { celsiusLabel.text = textField.text }
+    //
+    // Update the celsius label
+    //
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value:celsiusValue.value))
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    //
+    // Check if field has text then check if value can be a double
+    //
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    //
+    // Dismiss keyboard if background is tapped
+    //
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
 }
